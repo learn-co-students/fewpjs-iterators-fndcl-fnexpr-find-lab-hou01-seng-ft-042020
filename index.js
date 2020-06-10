2622d9f1366d57c4fd4293156a48bef11782255b
Stephanie Cheney <steph@scheneyphotography.com>
2020-06-10 14:44:53 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,21 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+// const record = [
+//   {year: "2018", result: "L"},
+//   {year: "2017", result: "W"},
+//   {year: "2016", result: "N/A"}
+// ]
+
+// function superbowlWin(record){
+//  let recordWin= record.find(function(record) {
+//     record.result === "W"
+//   }
+//   return !!recordWin ? recordWin.year : undefined 
+//   )
+// }
+
+superbowlWin = (record) => {
+  let result = record.find( record => record.result === "W" )
+  return !!result ? result.year : undefined
+}
