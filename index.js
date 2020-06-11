b325b520d7e5af24e3641512d2309ee09966822e
Krysta110 <62581900+Krysta110@users.noreply.github.com>
2020-06-10 23:32:41 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+superbowlWin = (record) => {
+  let win =  record.find(rec => rec.result === "W");
+  return win ? win.year : undefined;
+};
